63a8aef44c2682e39eebc5abd1f3e69e32bc9a7b
mardibak <mardibaki@gmail.com>
2025-05-02 10:38:25 +0330

    Add project files.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+#region twoSum
+using LeetCode;
+
+TwoSum twosum = new TwoSum();
+
+Console.WriteLine("Please enter array numbers separated by spaces (example: 1 2 3 4):");
+string numbersInput = Console.ReadLine();
+
+Console.WriteLine("Please enter the target amount:");
+string targetInput = Console.ReadLine();
+
+try
+{
+    // تبدیل رشته ورودی اعداد به آرایه ای از اعداد صحیح
+    int[] numbers = numbersInput.Split(' ')
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+    // تبدیل رشته ورودی هدف به عدد صحیح
+    int target = int.Parse(targetInput);
+
+    Console.WriteLine("nums : [" + string.Join(", ", numbers) + "]");
+    Console.WriteLine("targ : " + target);
+
+    // فراخوانی متد Zweisum و دریافت نتیجه
+    int[] resultIndices = twosum.Zweisum(numbers, target);
+
+    // چاپ نتیجه
+    Console.WriteLine($"Indexes of numbers with sum {target}: [{resultIndices[0]}, {resultIndices[1]}]");
+}
+catch (FormatException)
+{
+    Console.WriteLine("The input format is invalid. Please enter the numbers and target correctly.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+#endregion
---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode;
+
+public class TwoSum
+{
+    public int[] Zweisum(int[] nums, int target)
+    {
+        for (int numA = 0; numA < nums.Length; numA++)
+        {
+            for (int numB = numA + 1; numB < nums.Length; numB++)
+            {
+                if (nums[numA] + nums[numB] == target) 
+                    return new int[] { numA, numB };
+            }
+        }
+        throw new Exception("no two sum solution found");
+    }
+}
